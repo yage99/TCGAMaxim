66bfbdab8d00ccee216b9a96ce05f7a989f920ee
Ya Zhang <zhangya@mail.ustc.edu.cn>
2017-11-30 19:00:52 +0800

    imporve printProgress

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
+
 
 # Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+def printProgressBar(iteration, total, prefix='', suffix='',
+                     decimals=1, length=50, fill='█', start_time=None):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -11,15 +14,27 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
         total       - Required  : total iterations (Int)
         prefix      - Optional  : prefix string (Str)
         suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        decimals    - Optional  : positive number of decimals in
+                                  percent complete (Int)
         length      - Optional  : character length of bar (Int)
         fill        - Optional  : bar fill character (Str)
     """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    percent = ("{0:." + str(decimals)
+               + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    sys.stdout.write('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
+    left_time_str = ""
+    if start_time is not None:
+        current_time = time.time()
+        left_time = ((current_time - start_time)
+                     / float(iteration) * (total - iteration))
+        left_time_str = ("%02d:%02d:%02d"
+                         % (int(left_time / 3600),
+                            int((left_time % 3600) / 60),
+                            int(left_time % 60)))
+    sys.stdout.write('\r%s |%s| %s%% %s %s'
+                     % (prefix, bar, percent, suffix, left_time_str))
     sys.stdout.flush()
     # Print New Line on Complete
-    if iteration == total: 
+    if iteration == total:
         print()
