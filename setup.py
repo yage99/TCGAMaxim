0a7dcf5116985f0ebd183af966277ab1e5092752
zhang ya <zhangya998@gmail.com>
2021-11-06 03:09:54 +0800

    finish docs

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='TCGAMaxim',
       description='Python utils for process tcga source data',
       author='Ya Zhang',
       author_email='zhangya@mail.ustc.edu.cn',
-      url='',
+      url='https://github.com/yage99/TCGAMaxim.git',
+      download_url=""
       packages=['TCGAMaxim'],
       )
